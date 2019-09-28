047b645bf99bb8674e9c044e5847eff1404a7f31
dahabu saidi <dahabusaidi@gmail.com>
2019-09-28 12:36:13 +0300

    introducing variables

--- App/javaScripts/intro.js
+++ App/javaScripts/intro.js
@@ -1,1 +1,10 @@
-alert( 'Hello, world!' );
+// Define Variable in modern way
+let message = "Hello Mkawa";
+
+// Define Variable in old way
+var name = 'Papaa Mukulu';
+
+let string = message + " my name is " + name
+
+// show variable content
+alert(string);
