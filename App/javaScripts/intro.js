e9e40452e524fddb604999dd295f3292dfce643a
dahabu saidi <dahabusaidi@gmail.com>
2019-10-12 22:13:17 +0300

    describe logical operators

--- App/javaScripts/intro.js
+++ App/javaScripts/intro.js
@@ -12,10 +12,21 @@ const myBirthday = '18.04.1982';
 let string = `${message}, ${name}`
 
 // show variable content
-// alert(string);
+alert(string);
 
-// logical operators
+/**
+ * Logical operators
+ * OR finds the first truth value and operates from left to right
+ * AND (&&) Represented by 2 ampersands, returns true if both operands are true and false otherwise
+ * AND always finds the first false
+ * ! NOT operand, this always returns the inverse value
+ * !! used to convert value to boleen type
+ */
 let hour = 8;
+let minute = 30;
 if (hour < 10 || hour > 19) {
     alert('office closed please come tumor')
 }
+if (hour == 8 && minute == 30) {
+    alert( 'The time is 12:30' );
+  }
