3ef8f1c3969e8252f644814b96a951c12b54e96f
dahabu saidi <dahabusaidi@gmail.com>
2019-10-19 20:35:36 +0300

    add callback function

--- App/javaScripts/functions.js
+++ App/javaScripts/functions.js
@@ -1,3 +1,11 @@
+/**
+ * 
+ * @param {*} age 
+ * functions are actions so their name is usually a verb
+ * should be brief and accurate as possible and describe what the axactly function to
+ * 
+ */
+
 function checkAge(age) {
     if (age > 18) {
       return true;
@@ -13,3 +21,20 @@ function checkAge(age) {
   } else {
     alert( 'Access denied' );
   }
+
+
+  function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+  
+  function showOk() {
+    alert( "You agreed." );
+  }
+  
+  function showCancel() {
+    alert( "You canceled the execution." );
+  }
+  
+  // usage: functions showOk, showCancel are passed as arguments to ask
+  ask("Do you agree?", showOk, showCancel);
